74c5faf1500dad8e7eda1dc55af762f4faf78e1c
Lewis Lawler <lewislawler98@gmail.com>
2024-01-29 18:01:29 +0000

    adds slider to projects

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function toggleMenu () {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 50, // Adjust this value to control the space between slides
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
+});
